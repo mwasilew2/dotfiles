e747805426ee03e2ae4e9893240c675fe46bc480
Michal Wasilewski <mwasilewski@gmx.com>
2016-10-22 08:15:29 +0100

    more dotfiles General:
    *
    
    Signed-off-by: Michal Wasilewski <mwasilewski@gmx.com>

---
+++ mount.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+truecrypt --mount ~/private/ext4_linux_only /media/truecrypt1
+#truecrypt --mount container --options="iocharset=utf8" /media/truecrypt1
---
+++ rsync_diff.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -vvahi --delete-after --dry-run weekly.1/ weekly.0/
