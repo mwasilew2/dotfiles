4270f3ba3effe322a3d16067c828b33893b5174f
Michal Wasilewski <mwasilewski@gmx.com>
2017-06-19 22:10:18 +0100

    remove path from mount script
    Signed-off-by: Michal Wasilewski <mwasilewski@gmx.com>

--- mount.sh
+++ mount.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-veracrypt --mount ~/private/ext4_linux_only /media/truecrypt1
+veracrypt --mount ~/private/ext4_linux_only
 # truecrypt --mount ~/private/ext4_linux_only /media/truecrypt1
 #truecrypt --mount container --options="iocharset=utf8" /media/truecrypt1
