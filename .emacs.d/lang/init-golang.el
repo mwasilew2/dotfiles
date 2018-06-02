af956a40c27952e52ae980428a5804ef54ce9a51
Michal Wasilewski <mwasilewski@gmx.com>
2018-06-02 14:11:29 +0200

    further golang customizations
    Signed-off-by: Michal Wasilewski <mwasilewski@gmx.com>

--- .emacs.d/lang/init-golang.el
+++ .emacs.d/lang/init-golang.el
@@ -30,18 +30,18 @@
 ;; (with-eval-after-load 'go-mode
    ;; (require 'go-autocomplete))
 
-;; (defun my-go-mode-hook ()
-;;   ; Call Gofmt before saving
-;;   (add-hook 'before-save-hook 'gofmt-before-save)
-;;   ; Customize compile command to run go build
-;;   (if (not (string-match "go" compile-command))
-;;       (set (make-local-variable 'compile-command)
-;;            "go build -v && go test -v && go vet"))
-;;   ; Godef jump key binding
-;;   (local-set-key (kbd "M-.") 'godef-jump)
-;;   (local-set-key (kbd "M-*") 'pop-tag-mark)
-;; )
-;; (add-hook 'go-mode-hook 'my-go-mode-hook)
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  )
+(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (use-package go-stacktracer
   :ensure t
@@ -57,7 +57,5 @@
   :ensure t
   :diminish go-eldoc
   )
-
-(add-hook 'before-save-hook 'gofmt-before-save)
 
 (provide 'init-golang)
