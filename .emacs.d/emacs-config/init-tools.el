bfb746206a90422ef7ac73f388a49334be85d4b3
Michal Wasilewski <mwasilewski@gmx.com>
2018-06-07 21:11:49 +0200

    emacs

--- .emacs.d/emacs-config/init-tools.el
+++ .emacs.d/emacs-config/init-tools.el
@@ -1,45 +1,54 @@
 (use-package avy
   :ensure t
-  :config
-  (global-set-key (kbd "M-g f") 'avy-goto-word-1))
+  :bind
+  (
+   ("M-g f" . avy-goto-word-1)
+   )
+  )
 
 (use-package eldoc
-  :ensure t)
+  :ensure t
+  )
 
 (use-package helm
-  ;; HELM - fuzzy matching, etc.
+  ;; https://writequit.org/eos/eos-helm.html
   :ensure t
-  :bind ( ;; First using helm for M-x so we get a live filter
-          ;; of options, and don't need to keep tab completing.
-          ("M-x" . helm-M-x)
-          ;; Also use helm for buffers. I can never remember the
-          ;; buffers I have open.
-          ("C-x C-b" . helm-buffers-list)
-          ;; Finding files can also be a pain, so use helm
-          ;; to locate and open files
-          ("C-x C-f" . helm-find-files))
+  :bind
+  ( ;; First using helm for M-x so we get a live filter
+    ;; of options, and don't need to keep tab completing.
+    ("M-x" . helm-M-x)
+    ;; Also use helm for buffers. I can never remember the
+    ;; buffers I have open.
+    ("C-x C-b" . helm-buffers-list)
+    ;; Finding files can also be a pain, so use helm
+    ;; to locate and open files
+    ("C-x C-f" . helm-find-files)
+  )
   :config
   (setq helm-boring-buffer-regexp-list (list
-                                        ;;(rx "*magit-")
-                                        ;;(rx "*magit: ")
-                                        ;;(rx "magit-")
-                                        ;;(rx "magit: ")
-                                        (rx "*helm ")
-                                        (rx "*Minibuf-")
-                                        (rx "*Echo Area")
+                                        ;; (rx "*magit-")
+                                        ;; (rx "*magit: ")
+                                        ;; (rx "magit-")
+                                        ;; (rx "magit: ")
+                                        ;; (rx "*helm ")
+                                        ;; (rx "*Minibuf-")
+                                        ;; (rx "*Echo Area")
 
-                                        (rx "*Backtrace*")
-                                        (rx "*code-converting-work*")
-                                        (rx "*code-conversion-work*")
-                                        (rx "*Compile-Log*")
-                                        (rx "*Completions*")
-                                        (rx "*groovy*")
-                                        (rx "*Help*")
-                                        (rx "*Messages*")
-                                        (rx "*NeoTree*")
-                                        (rx "*scratch*")
-                                        (rx "*server*")
-                                        (rx "*Shell Command Output*"))))
+                                        ;; (rx "*Backtrace*")
+                                        ;; (rx "*code-converting-work*")
+                                        ;; (rx "*code-conversion-work*")
+                                        ;; (rx "*Compile-Log*")
+                                        ;; (rx "*Completions*")
+                                        ;; (rx "*groovy*")
+                                        ;; (rx "*Help*")
+                                        ;; (rx "*Messages*")
+                                        ;; (rx "*NeoTree*")
+                                        ;; (rx "*scratch*")
+                                        ;; (rx "*server*")
+                                        ;; (rx "*Shell Command Output*")
+                                        )
+        )
+  )
 
 (use-package helm-swoop
   :ensure t
@@ -99,20 +108,24 @@
   ;; replaced move-text
   :ensure t
   :config
-  (global-smart-shift-mode 1))
+  (global-smart-shift-mode 1)
+  )
 
-(use-package tabbar
-  :ensure t)
-(defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
-  "Returns the name of the tab group names the current buffer belongs to.
-  There are two groups: Emacs buffers (those whose name starts with '*', plus
-  dired buffers), and the rest.  This works at least with Emacs v24.2 using
-  tabbar.el v1.7."
-  (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
-       ((eq major-mode 'dired-mode) "emacs")
-       (t "user"))))
-(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
-(tabbar-mode 1)
+;; (use-package tabbar-ruler
+;;   :ensure t)
+
+;; (use-package tabbar
+;;   :ensure t)
+;; (defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
+;;   "Returns the name of the tab group names the current buffer belongs to.
+;;   There are two groups: Emacs buffers (those whose name starts with '*', plus
+;;   dired buffers), and the rest.  This works at least with Emacs v24.2 using
+;;   tabbar.el v1.7."
+;;   (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+;;        ((eq major-mode 'dired-mode) "emacs")
+;;        (t "user"))))
+;; (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
+;; (tabbar-mode 1)
 
 (use-package yasnippet
   :ensure t
