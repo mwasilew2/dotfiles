2bb368df0755b94869c24c70f75e3f3e2eae709a
Michal Wasilewski <mwasilewski@gmx.com>
2019-12-09 22:53:00 +0100

    change enc volume path
    Signed-off-by: Michal Wasilewski <mwasilewski@gmx.com>

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-veracrypt --mount ~/private/ext4_linux_only
+veracrypt --mount ~/private/personal/ext4_linux_only
 # truecrypt --mount ~/private/ext4_linux_only /media/truecrypt1
 #truecrypt --mount container --options="iocharset=utf8" /media/truecrypt1
