af790841e478aca55564eda624b3aa38c82d6c86
Michal Wasilewski <mwasilewski@gmx.com>
2023-06-16 18:33:53 +0200

    add command for read only mounts
    Signed-off-by: Michal Wasilewski <michal@mwasilewski.net>

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 veracrypt --mount /home/michal/private/personal/ext4_linux_only
+#veracrypt -m ro --mount /home/michal/private/personal/ext4_linux_only
 # truecrypt --mount ~/private/ext4_linux_only /media/truecrypt1
 #truecrypt --mount container --options="iocharset=utf8" /media/truecrypt1
